f51d7ed9f417d9eaa5a4a707e5e85b2767560615
wantysal <salome.wanty@gmail.com>
2021-02-11 14:45:27 +0100

    [NF] calculation of bandwidth centered on fc in bark/ERB

---
+++ mosqito/functions/shared/bandwidth.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 15:12:05 2021
+
+@author: wantysal
+"""
+
+def bandwidth(fc, bandtype):
+    """
+        Calculate the critical bandwidth of the Bark or ERB band centered on fc
+        
+        For Bark scale, the relation is bw = bw = 25 + 75 ( 1+1.4e-06} fc**2 )**0.69
+        ref [1]
+        
+        For ERb scale, the relation is bw = 24.7 + fc/9.265
+        ref[2]
+        
+      References :
+          
+      [1] E. Zwicker and E. Terhardt. Analytical expressions for criticalband
+     rate and critical bandwidth as a function of frequency. The Journal of
+     the Acoustical Society of America, 68(5):1523--1525, 1980.
+        
+      [2] B. R. Glasberg and B. Moore. Derivation of auditory filter shapes from
+     notched-noise data. Hearing Research, 47(1-2):103, 1990.
+    
+    Parameters
+    ----------
+    fc : list on float
+        center frequencies of the band to evaluate
+    type : string
+        'bark' or 'erb'
+        
+    Returns
+    -------
+    bw : float
+        bandwidth of the band centered on fc
+        
+        
+    """
+
+    if bandtype == 'erb':
+        bw = 24.7 + fc/9.265
+    
+    if bandtype == 'bark':
+        bw = 25 + 75  *(1 + 1.4e-06*fc**2)**0.69
+        
+    return bw
+
